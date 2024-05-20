bd1c0bfa44822bc57f69943d9c4a5879ff0c3760
wldnn021200 <welldonestoree@gmail.com>
2024-05-20 11:19:14 +0700

    JVscript.js

--- JVscript.js
+++ JVscript.js
@@ -1,4 +1,4 @@
-function calculate() {
+function calculateSum() {
     const input1 = document.getElementById('input1').value;
     const input2 = document.getElementById('input2').value;
     const result = parseFloat(input1) + parseFloat(input2);
